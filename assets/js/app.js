06e63c314d65e1e85939cfb0243dc4e1c0326396
lccovington <lacie.covington@gmail.com>
2022-02-01 03:54:47 -0500

    added scroll to top button

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// scroll to top functionality
+
+const scrollUp = document.getElementById("scroll-up");
+
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+});
